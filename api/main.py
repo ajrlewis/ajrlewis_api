e3e6d1fe44410db068e4105a0c37050d47095068
ajrlewis <hello@ajrlewis.com>
2024-06-19 10:25:21 +0100

    Added webscraping and chat routes.

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .database import Base, engine
-from .routers import index  # , chat, web_scraper
+from .routers import index, chat, web_scraper
 
 Base.metadata.create_all(bind=engine)
 
@@ -21,5 +21,5 @@ app = FastAPI()
 
 
 app.include_router(index.router)
-# app.include_router(chat.router)
-# app.include_router(web_scraper.router)
+app.include_router(chat.router)
+app.include_router(web_scraper.router)
