cfee8b096b5f936bc5cd4cbf92deb824ec1cd0b1
ajrlewis <hello@ajrlewis.com>
2024-06-21 14:13:57 +0100

    Trying to get scrape_dynamic_website_for_text to work on Vercel .. again and again and again

--- api/utils/web_scraper.py
+++ api/utils/web_scraper.py
@@ -108,11 +108,31 @@ def scrape_dynamic_website_for_text(url: str) -> tuple[str]:
     # chrome_options.add_argument("--ignore-certificate-errors")
     # chrome_options.add_argument("--no-sandbox")
     # chrome_options.add_argument("disable-notifications")
-    driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager(driver_version="2.26").install()),
-        options=chrome_options,
-        seleniumwire_options={"request_storage_base_dir": "/tmp"},
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1280x1696")
+    chrome_options.add_argument("--user-data-dir=/tmp/user-data")
+    chrome_options.add_argument("--hide-scrollbars")
+    chrome_options.add_argument("--enable-logging")
+    chrome_options.add_argument("--log-level=0")
+    chrome_options.add_argument("--v=99")
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument("--data-path=/tmp/data-path")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--homedir=/tmp")
+    chrome_options.add_argument("--disk-cache-dir=/tmp/cache-dir")
+    chrome_options.add_argument(
+        "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
     )
+    chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    # driver = webdriver.Chrome(
+    #     service=ChromeService(ChromeDriverManager(driver_version="2.26").install()),
+    #     options=chrome_options,
+    #     seleniumwire_options={"request_storage_base_dir": "/tmp"},
+    # )
 
     # from seleniumwire import webdriver
     # from selenium.webdriver.chrome.service import Service
