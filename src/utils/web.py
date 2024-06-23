7889d36fb756d93f254243baa14df233722776d6
ajrlewis <hello@ajrlewis.com>
2024-06-23 13:14:41 +0100

    Vercel again ... and again.

--- src/utils/web.py
+++ src/utils/web.py
@@ -133,7 +133,8 @@ def scrape_dynamic_website_for_text(url: str) -> tuple[str]:
     # chrome_options.add_argument(f"user-agent={headers['User-Agent']}")
     # driver = webdriver.Chrome(options=chrome_options)
 
-    executable_path = rf"{os.getcwd()}/../geckodriver"
+    # executable_path = rf"{os.getcwd()}    /"
+    executable_path = rf"{os.getcwd()}/geckodriver"
     print(
         f"{__name__}.scrape_dynamic_website_for_text",
         "executable_path = ",
@@ -141,7 +142,9 @@ def scrape_dynamic_website_for_text(url: str) -> tuple[str]:
     )
 
     print("")
-    # print(os.listdir(os.getcwd()))
+    print(os.listdir(os.getcwd()))
+    print("")
+    print("os.path.isfile(executable_path) = ", os.path.isfile(executable_path))
     import platform
 
     print("os.name = ", os.name)
@@ -171,7 +174,9 @@ def scrape_dynamic_website_for_text(url: str) -> tuple[str]:
     # firefox_options.add_argument("--disk-cache-dir=/tmp/cache-dir")
     headers = get_random_headers()
     firefox_options.add_argument(f"user-agent={headers['User-Agent']}")
-    firefox_options.executable_path = os.getcwd()
+    # firefox_options.executable_path = os.getcwd()
+    # firefox_options.binary_location = executable_path
+
     print(
         f"{__name__}.scrape_dynamic_website_for_text",
         "firefox_options = ",
